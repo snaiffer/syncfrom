4aba6497cd81ca1cac7acb9f33622ced1dc237af
Alexander Danilov <snaiffer@gmail.com>
2016-01-19 23:11:40 +0300

    added short_sync option

--- install.sh
+++ install.sh
@@ -39,9 +39,25 @@ get_disk_name() {
   done
   }
 
+short_sync() {
+  printf "Do you want the short sync (include sync of several folders)? (y/n) " && read answer
+  if [ "$answer" = "y" ]; then
+    short_sync="BasKet documents work unsorted"
+    printf "\tShort sync turn on. Default list for sync: ($short_sync) " && read new_short_sync
+    if [ "$new_short_sync" != "" ]; then short_sync="$new_short_sync"; fi
+    #analize && break
+  fi
+  }
+
 save_conf() {
   sudo echo "export path_pc=$path_pc
 export path_disk=$path_disk" > $file_conf
+  if [ "$short_sync" != "" ]; then
+    sudo echo "" >> $file_conf
+    sudo echo "# Settings for selective synchronization" >> $file_conf
+    sudo echo "# write folders names through space; or delete this settings for full sync" >> $file_conf
+    sudo echo "export short_sync=\"$short_sync\"" >> $file_conf
+  fi
   }
 
 
@@ -51,6 +67,7 @@ sudo cp -f $dir_script/syncfrom* $dir_bin/ && sudo chmod +x $dir_bin/syncfrom*
 echo "Settings:"
 get_path_pc
 get_disk_name
+short_sync
 save_conf
 
 echo -e "done.\n"
